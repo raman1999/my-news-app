9753ffe0fc17f29763862a73316e833c96d88e0a
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-01-07 11:09:38 -0800

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -19,10 +19,12 @@ const News = (props)=>{
         props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`; 
         setLoading(true)
+        console.log(url);
         let data = await fetch(url);
         props.setProgress(30);
         let parsedData = await data.json()
         props.setProgress(70);
+        console.log(parsedData)
         setArticles(parsedData.articles)
         setTotalResults(parsedData.totalResults)
         setLoading(false)
